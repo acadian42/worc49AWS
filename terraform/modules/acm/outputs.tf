67e87f67c7ce273885de139cfe75144d8e2155da
acadian42 <netfeline2@gmail.com>
2026-05-13 08:31:26 -0400

    .

--- terraform/modules/acm/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "certificate_arn" {
-  description = "ARN of the ACM certificate to attach to the ALB"
-  value = (
-    var.certificate_mode == "import"
-    ? var.imported_certificate_arn
-    : aws_acm_certificate.tines[0].arn
-  )
-}
--- terraform/modules/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repo_urls" {
-  description = "Map of short name to ECR repository URL"
-  value       = { for k, v in aws_ecr_repository.repo : k => v.repository_url }
-}
-
-output "repo_arns" {
-  description = "Map of short name to ECR repository ARN"
-  value       = { for k, v in aws_ecr_repository.repo : k => v.arn }
-}
